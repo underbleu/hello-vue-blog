9de3c215f2bd604786fb57c646d1d658fa065a4e
bong kim <32095134+underbleu@users.noreply.github.com>
2018-03-18 21:33:54 +0900

    리팩토링 - 컴포넌트명 변경

---
+++ src/components/Hello.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <div v-for="post in postList">
+      <img :src="post.image" v-if="post.image" />
+      <router-link :to="`/post/${ post._id }`">
+        {{ post.title }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  components: {
+    
+  },
+  beforeMount () {
+    
+  },
+  mounted () {
+    this.getList()
+  },
+  data () {
+    return {
+      postList: []
+    }
+  },
+  props: {
+    
+  },
+  methods: {
+    getList: function () {
+      this.$http.get(`http://localhost:3000/post`)
+        .then((res) => {
+          console.log(res)
+          this.postList = res.data
+        })
+    }
+  },
+  computed: {
+    
+  },
+  watch: {
+    
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  
+}
+</style>
