d827c8b8ab207c7e39001074d62ca91333c117b9
bong kim <32095134+underbleu@users.noreply.github.com>
2018-03-18 21:32:23 +0900

    기능 추가 - 글쓰기 등록 버튼 기능 구현

--- src/components/Write.vue
+++ src/components/Write.vue
@@ -4,6 +4,7 @@
     <input v-moder="image" placeholder="이미지" />
     <textarea v-model="markdown"></textarea>
     <div v-html="md2html"></div>
+    <button @click.once="save">저장</button>
   </div>
 </template>
 
@@ -32,7 +33,17 @@ export default {
     
   },
   methods: {
-    
+    save: function () {
+      let post = {
+        title: this.title,
+        image: this.image,
+        markdown: this.markdown
+      }
+      this.$http.post(`http://localhost:3000/post`, post)
+        .then((res) => {
+          this.$router.push('/') // 글이 저장되면 Home 화면으로 이동
+        })
+    }
   },
   computed: {
     md2html: function () {
