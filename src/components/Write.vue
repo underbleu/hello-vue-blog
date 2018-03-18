7c78304341fbba40020d6655cea64b9cb7b64046
bong kim <32095134+underbleu@users.noreply.github.com>
2018-03-18 20:35:53 +0900

    기능 추가 - 글쓰기

---
+++ src/components/Write.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="write">
+    <input v-model="title" placeholder="제목" />
+    <input v-moder="image" placeholder="이미지" />
+    <textarea v-model="markdown"></textarea>
+    <div v-html="md2html"></div>
+  </div>
+</template>
+
+<script>
+import marked from 'marked'
+
+export default {
+  name: 'write',
+  components: {
+    
+  },
+  beforeMount () {
+    
+  },
+  mounted () {
+    
+  },
+  data () {
+    return {
+      title: '',
+      image: '',
+      markdown: ''
+    }
+  },
+  props: {
+    
+  },
+  methods: {
+    
+  },
+  computed: {
+    md2html: function () {
+      return marked(this.markdown)
+    }
+  },
+  watch: {
+    
+  }
+}
+</script>
+
+<style scoped>
+.write {
+  
+}
+</style>
